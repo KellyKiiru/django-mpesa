058890f3bfebf59c36dce13c6823a4f861135dbb
kellykiiru <infowithkiiru@gmail.com>
2023-09-28 22:23:15 +0300

    add onto TestView class

--- daraja/views.py
+++ daraja/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .access_token import generate_access_token
+from .utils import timestamp_conversion
+from  .encode_base64 import generate_password
 
 def index(request):
     return HttpResponse("sfneo")
@@ -12,7 +14,9 @@ class TestView(APIView):
 
     def get(self, request, format=None):
         access_token = generate_access_token()
+        formatted_time = timestamp_conversion()
+        decoded_password = generate_password(formatted_time)
         
-        return Response({"access_token": access_token})
+        return Response({"access_token": access_token, "password ": decoded_password})
     
     
