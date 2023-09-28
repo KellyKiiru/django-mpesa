ccead8228660cc24ad28e0320e732b155d61fadd
kellykiiru <infowithkiiru@gmail.com>
2023-09-28 23:55:51 +0300

    define url path for MakePayment class view

--- daraja/urls.py
+++ daraja/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import TestView,index
+from .views import TestView,index, MakePayment
 
 urlpatterns = [
     path('', TestView.as_view(), name='testview'),
     path('index/', views.index, name='index'),
+    path('api/v1/lnm_stk_push/', MakePayment.as_view(), name='makepayment'),
 ]
