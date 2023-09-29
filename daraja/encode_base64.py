bbfed57d46ed76f035d11233cc1b8653e8d68f5e
kellykiiru <infowithkiiru@gmail.com>
2023-09-29 19:58:07 +0300

    refine method

--- daraja/encode_base64.py
+++ daraja/encode_base64.py
@@ -1,16 +1,27 @@
 import base64
 from django.conf import settings
 
+# def generate_password(date):
+#     # Convert settings.LIPANAMPESA_PASSKEY and settings.BUSINESS_SHORT_CODE to strings
+#     passkey = str(settings.LIPANAMPESA_PASSKEY)
+#     business_short_code = str(settings.BUSINESS_SHORT_CODE)
+    
+#     data_to_encode = passkey + date + business_short_code
+    
+#     encoded_password = base64.b64encode(data_to_encode.encode(('utf-8')))
+    
+#     return encoded_password
+
 def generate_password(date):
     # Convert settings.LIPANAMPESA_PASSKEY and settings.BUSINESS_SHORT_CODE to strings
     passkey = str(settings.LIPANAMPESA_PASSKEY)
     business_short_code = str(settings.BUSINESS_SHORT_CODE)
     
-    # Concatenate the strings
     data_to_encode = passkey + date + business_short_code
     
-    # Encode the concatenated string using base64
-    encoded_password = base64.b64encode(data_to_encode.encode('utf-8'))
+    encoded_string = base64.b64encode(data_to_encode.encode())
+    
+    encoded_password = encoded_string.decode("utf-8")
     
     return encoded_password
 
