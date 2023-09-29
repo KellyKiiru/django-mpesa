64df9eea0899e214cc26c0f75c64988c45e57c1e
kellykiiru <infowithkiiru@gmail.com>
2023-09-29 19:57:10 +0300

    urls

--- daraja/urls.py
+++ daraja/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import TestView,index, MakePayment
 
 urlpatterns = [
-    path('', TestView.as_view(), name='testview'),
-    path('index/', views.index, name='index'),
+    path('test', TestView.as_view(), name='testview'),
+    path('', views.index, name='index'),
     path('api/v1/lnm_stk_push/', MakePayment.as_view(), name='makepayment'),
 ]
